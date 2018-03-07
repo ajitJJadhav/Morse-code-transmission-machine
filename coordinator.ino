bae2aa9186a0e38a2b10976a6400b2b506d858b2
Ajit Jadhav <ajitopensource@gmail.com>
2018-03-07 14:14:03 +0530

    Final Software

---
+++ coordinator.ino
@@ -0,0 +1,134 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial xbee(2, 3); // RX, TX
+char morse[10],c;
+int i=0;
+String S="";
+void setup()
+{
+    Serial.begin(9600);
+
+    Serial.println( "Arduino started receiving bytes via XBee" );
+
+    // Set the data rate for the SoftwareSerial port.
+    xbee.begin(9600);
+}
+
+void loop()  {  
+  if(xbee.available())
+    {  
+      c = xbee.read();
+      
+      if(c == ' ' || c == '\n')
+        Serial.print(c);
+      
+      //Serial.println(c);
+      else if(c != '~')
+      {
+        morse[i]=c;
+        //Serial.println(morse[i]);
+         i++;
+      }
+      else if( c == '~')
+      {
+         //Serial.println(c);
+        morse[i] = '\0';
+        i = 0;
+        
+        S="";
+        while(morse[i] != '\0')
+        {
+          S = S +  morse[i];
+         //Serial.print("1");
+          //Serial.print(S);
+          i++;
+        }
+       i=0;
+      // Serial.println(S);
+      printaj(S);
+    }
+    }
+    //Serial.print("Character received:");
+    //Serial.println(temp);
+    //delay(1000);
+}
+
+void printaj(String prevodilac)   //ugly part of the code but it works fine
+{                                 //compare morse string to known morse values and print out the letter or a number 
+                                  //the code is written based on the international morse code, one thing i changed is that insted of typing a special string to end the line it happens with enough delay  
+  if (prevodilac=="*-")
+    Serial.print("A");
+  else if (prevodilac=="-***")  
+    Serial.print("B");
+  else if (prevodilac=="-*-*")  
+    Serial.print("C");
+  else if (prevodilac=="-**")  
+    Serial.print("D");
+  else if (prevodilac=="*")  
+    Serial.print("E");
+  else if (prevodilac=="**-*")  
+    Serial.print("F");
+  else if (prevodilac=="--*")  
+    Serial.print("G");
+  else if (prevodilac=="****")  
+    Serial.print("H");
+  else if (prevodilac=="**")  
+    Serial.print("I");
+  else if (prevodilac=="*---")  
+    Serial.print("J");
+  else if (prevodilac=="-*-")  
+    Serial.print("K");
+  else if (prevodilac=="*-**")  
+    Serial.print("L");
+  else if (prevodilac=="--")  
+    Serial.print("M");
+  else if (prevodilac=="-*")  
+    Serial.print("N");
+  else if (prevodilac=="---")  
+    Serial.print("O");
+  else if (prevodilac=="*--*")  
+    Serial.print("P");
+  else if (prevodilac=="--*-")  
+    Serial.print("Q");
+  else if (prevodilac=="*-*")  
+    Serial.print("R");
+  else if (prevodilac=="***")  
+    Serial.print("S");
+  else if (prevodilac=="-")  
+    Serial.print("T");
+  else if (prevodilac=="**-")  
+    Serial.print("U");
+  else if (prevodilac=="***-")  
+    Serial.print("V");
+  else if (prevodilac=="*--")  
+    Serial.print("W");
+  else if (prevodilac=="-**-")  
+    Serial.print("X");
+  else if (prevodilac=="-*--")  
+    Serial.print("Y");
+  else if (prevodilac=="--**")  
+    Serial.print("Z");
+
+  else if (prevodilac=="*----")  
+    Serial.print("1");
+  else if (prevodilac=="**---")  
+    Serial.print("2");
+  else if (prevodilac=="***--")  
+    Serial.print("3");
+  else if (prevodilac=="****-")  
+    Serial.print("4");
+  else if (prevodilac=="*****")  
+    Serial.print("5");
+  else if (prevodilac=="-****")
+    Serial.print("6");
+  else if (prevodilac=="--***")  
+    Serial.print("7");
+  else if (prevodilac=="---**")  
+    Serial.print("8");
+  else if (prevodilac=="----*")  
+    Serial.print("9");
+  else if (prevodilac=="-----")  
+    Serial.print("0");
+    
+  prevodilac=""; 
+}
---
+++ router.ino
@@ -0,0 +1,108 @@
+#include <SoftwareSerial.h>
+SoftwareSerial xbee(2,3); // RX, TX
+
+const int buttonPin = 5;    
+
+int buttonState = LOW;             
+int lastButtonState = LOW;  
+
+int pause_value = 250;  // depending on your skill and how fast your fingers are you can change this value to make typing a message faster or slower
+long signal_length = 0;
+long pause = 0;
+
+String morse="";
+String dash="-";
+String dot="*";
+
+boolean cheker = false;
+boolean linecheker = false;
+boolean spacechecker = false;
+
+long lastDebounceTime = 0;  
+long debounceDelay = 50;
+
+char c;
+    
+void setup()
+{
+  Serial.begin(9600);
+  xbee.begin(9600);
+  pinMode(buttonPin, INPUT);
+  
+  while(!digitalRead(buttonPin))
+    ;
+  
+}
+
+void loop() {
+ 
+  buttonState = digitalRead(buttonPin);
+
+  if (buttonState && lastButtonState)       // basic state machine depending on the state of the signal from the button
+  {
+    ++signal_length;       
+  }
+  else if(!buttonState && lastButtonState)          //this part of the code happens when the button is released and it send either * or - into the buffer
+  {
+     if (signal_length>50 && signal_length<2*pause_value )
+     {
+       morse =  morse + dot;
+     } 
+      else if (signal_length>2*pause_value)
+      {
+        morse = morse +  dash;
+      }
+    signal_length=0; 
+    }
+  else if(buttonState && !lastButtonState)          // this part happens when the button is pressed and its use to reset several values
+  {
+    pause=0;
+    cheker = true;
+    linecheker = true;
+    spacechecker = true;
+  }
+  else if (!buttonState && !lastButtonState)
+  {  
+    ++pause;
+    if (( pause>3*pause_value ) && (cheker))
+    { 
+      //printaj(morse);
+      //morse = morse + '\0';
+      int i=0;
+      
+      while(morse[i] != '\0')
+       {
+        c = morse[i];
+       // Serial.print(c);
+        xbee.write(c);
+        i++;
+       }
+       c = '~';
+       xbee.write(c);
+      cheker = false;
+      morse = ""; 
+      //xbee.write(morse);
+    }
+    
+    if ((pause>6*pause_value) && (spacechecker))
+    {
+      //Serial.print(" ");
+      c = ' ';
+      xbee.write(c);
+      spacechecker = false;
+    }
+    
+    if ((pause>15*pause_value) && (linecheker))
+    {
+      //Serial.println();
+      c = '\n';
+      xbee.write(c);
+      linecheker = false;
+    }
+  }
+  lastButtonState=buttonState;
+  delay(1);
+  
+  //Serial.println(morse);
+}
+
